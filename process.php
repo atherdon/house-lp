1ab0525642b969cd5177ad352c4b59966ee64bee
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-07-11 05:57:02 -0700

    +

--- process.php
+++ process.php
@@ -4,7 +4,7 @@ $name    = strip_tags($_POST['name']);
 $email   = strip_tags($_POST['email']);
 // $message = strip_tags($_POST['message']);
 // $budget  = strip_tags($_POST['budget']);
-
+6LfldygUAAAAADNKDGy1PqWtUTQV_nf9y0RPalIb
 // Send Message
 mail( "mail@example.com", "Contact Form Submission",
 "Name: $name\nEmail: $email\nBudget: $budget$\nMessage: $message\n",
