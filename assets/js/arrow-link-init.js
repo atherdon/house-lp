7087ba8f56ff776893e125b05b96e75cf9c50610
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-07-20 02:53:21 +0300

    updates

---
+++ assets/js/arrow-link-init.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+
+	if($('.scrollto').length) {
+		$('.scrollto').click(function() {
+			$.scrollTo($(this).attr("href"), {duration: 1000, easing:'swing'});
+			return false;
+		})
+	}
+	if($('.scrolltoslow').length) {
+		$('.scrolltoslow').click(function() {
+			$.scrollTo($(this).attr("href"), {duration: 2000});
+			return false;
+		})
+	}
+
+
+var waypoint = new Waypoint({
+  element: $('.left-column h1'),
+  handler: function(direction) {
+    console.log('Scrolled to waypoint!')
+
+    $('a.down').fadeIn();
+    
+  },
+  // offset: '600px'
+})
+
+
+var waypoint = new Waypoint({
+  element: $('.left-column h2'),
+  handler: function(direction) {
+    console.log('Scrolled to waypoint!')
+
+    $('a.down').fadeOut();
+
+  },
+  // offset: '600px'
+})
+
+// var inview = new Waypoint.Inview({
+//   element: $('#start'),
+//   enter: function(direction) {
+//     notify('Enter triggered with direction ' + direction)
+//   },
+//   entered: function(direction) {
+//     notify('Entered triggered with direction ' + direction)
+//   },
+//   exit: function(direction) {
+//     notify('Exit triggered with direction ' + direction)
+//   },
+//   exited: function(direction) {
+//     notify('Exited triggered with direction ' + direction)
+//   }
+// });
+
+	/* Waypoints on the arrowdown link - make it dissapear when we're not on #start */
+	// function arrowdown() {
+
+	// 	console.log('0');
+
+	// 	if(windowWidth >= 480) {
+	// 		$('#start').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+
+	// 			console.log('123');
+	// 		  if (isInView) {
+	// 		    if (visiblePartY == 'bottom') {
+	// 		      $('a.down').fadeOut();
+	// 		    } 
+	// 		    else {
+	// 		      $('a.down').fadeIn();
+	// 		    } 
+	// 		  } 
+	// 		});
+	// 	}
+	// }
+	// arrowdown();
+
+	// $(window).resize(function(){
+	// 	// setHeaderHeight();
+	// 	// if(windowWidth > 480) {
+			
+	// 	// }
+	// 	// else {
+	// 	// 	$('.subheader-container .wrapper').css('height', 'auto' );
+	// 	// }
+	// 	arrowdown();
+	// 	// setNavTopOffset();
+	// })
+
+});
