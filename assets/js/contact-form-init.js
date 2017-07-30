edadf125e16c637ab0cce94a7ea848772ea67cdb
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-07-30 22:21:37 +0300

    +

--- assets/js/contact-form-init.js
+++ assets/js/contact-form-init.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
 
-    if($('.formvalidation').length) {
-        $(".formvalidation").validate({
+    /* Contact Form */
+    if($('#contactForm').length) {
+
+        $("#contactForm").validate({
             invalidHandler: function(event, validator) {
               // 'this' refers to the form
               var errors = validator.numberOfInvalids();
               if (errors) {
+
                 var message = errors == 1
                   ? 'You missed 1 field. It has been highlighted'
                   : 'You missed ' + errors + ' fields. They have been highlighted';
+
                 $("div.error").html(message);
                 $("div.error").fadeIn();
               } else {
@@ -35,4 +39,127 @@ $(document).ready(function() {
         
         });
     }
+
+    /* Modal1 */
+    if($('#FormModal1').length) {
+
+        $("#FormModal1").validate({
+            invalidHandler: function(event, validator) {
+              // 'this' refers to the form
+              var errors = validator.numberOfInvalids();
+              if (errors) {
+                
+                var message = errors == 1
+                  ? 'You missed 1 field. It has been highlighted'
+                  : 'You missed ' + errors + ' fields. They have been highlighted';
+
+                $("div.error").html(message);
+                $("div.error").fadeIn();
+              } else {
+                $("div.error").hide();
+              }
+            },
+            showErrors: false,
+            errorPlacement: function(error,element) {
+                return true;
+            },
+            submitHandler: function(form) { 
+                jQuery(form).ajaxSubmit({
+                    url: 'process.php', 
+                    success: function() {
+                        var message = 'Thank\'s for your message. We\'ll answer as soon as possible.';
+                        $("div.error").html(message);
+                        $("div.error").fadeIn();
+                    },
+                    error: function() {
+                        alert('Something`s wrong.');
+                    }
+                });
+            }
+        
+        });
+    }
+
+    /* Modal2 */
+    if($('#FormModal21').length) {
+
+        $("#FormModal21").validate({
+            invalidHandler: function(event, validator) {
+              // 'this' refers to the form
+              var errors = validator.numberOfInvalids();
+              if (errors) {
+                
+                var message = errors == 1
+                  ? 'You missed 1 field. It has been highlighted'
+                  : 'You missed ' + errors + ' fields. They have been highlighted';
+
+                $("div.error").html(message);
+                $("div.error").fadeIn();
+              } else {
+                $("div.error").hide();
+              }
+            },
+            showErrors: false,
+            errorPlacement: function(error,element) {
+                return true;
+            },
+            submitHandler: function(form) { 
+                jQuery(form).ajaxSubmit({
+                    url: 'process.php', 
+                    success: function() {
+                        var message = 'Thank\'s for your message. We\'ll answer as soon as possible.';
+                        $("div.error").html(message);
+                        $("div.error").fadeIn();
+                    },
+                    error: function() {
+                        alert('Something`s wrong.');
+                    }
+                });
+            }
+        
+        });
+    }    
+
+
+    /* Modal3 */
+    if($('#FormModal31').length) {
+
+        $("#FormModal31").validate({
+            invalidHandler: function(event, validator) {
+              // 'this' refers to the form
+              var errors = validator.numberOfInvalids();
+              if (errors) {
+                
+                var message = errors == 1
+                  ? 'You missed 1 field. It has been highlighted'
+                  : 'You missed ' + errors + ' fields. They have been highlighted';
+
+                $("div.error").html(message);
+                $("div.error").fadeIn();
+              } else {
+                $("div.error").hide();
+              }
+            },
+            showErrors: false,
+            errorPlacement: function(error,element) {
+                return true;
+            },
+            submitHandler: function(form) { 
+                jQuery(form).ajaxSubmit({
+                    url: 'process.php', 
+                    success: function() {
+                        var message = 'Thank\'s for your message. We\'ll answer as soon as possible.';
+                        $("div.error").html(message);
+                        $("div.error").fadeIn();
+                    },
+                    error: function() {
+                        alert('Something`s wrong.');
+                    }
+                });
+            }
+        
+        });
+    }    
+    
+
 });
