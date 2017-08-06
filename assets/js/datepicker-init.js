1760fc28730c3ef6847debcae5d0b2adb07f0c71
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-08-06 06:03:15 +0300

    +

--- assets/js/datepicker-init.js
+++ assets/js/datepicker-init.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
 
 
-	$.map( $('.datepicker-input'), function( val, i ) {
+	// $.map( $('.datepicker-input'), function( val, i ) {
 
-	   	$(val).datepicker({
-			language: "ru",
-			orientation: "bottom"
-			});
-	});
+	//    	$(val).datepicker({
+	// 		language: "ru",
+	// 		orientation: "bottom"
+	// 		});
+	// });
 
 
 
-	var options = {
-		show2400: true,
-		timeFormat: 'H:i'
-		// time from & to
-	};
+	// var options = {
+	// 	show2400: true,
+	// 	timeFormat: 'H:i'
+	// 	// time from & to
+	// };
 
 	
-	$.map( $('.timepicker-input'), function( val, i ) {
+	// $.map( $('.timepicker-input'), function( val, i ) {
 
-	   	$(val).timepicker(options);
-	});
+	//    	$(val).timepicker(options);
+	// });
 
-	$.map( $("select[name='month']"), function( val, i ) {
+	// $.map( $("select[name='month']"), function( val, i ) {
 
 
-	   	$(val).select2({
-	  		placeholder: function(){
-		        $(val).data('placeholder');
-		    },
-	  		minimumResultsForSearch: Infinity,
-	  		 width: 'element'
-		});
+	//    	$(val).select2({
+	//   		placeholder: function(){
+	// 	        $(val).data('placeholder');
+	// 	    },
+	//   		minimumResultsForSearch: Infinity,
+	//   		 width: 'element'
+	// 	});
 
-	});
+	// });
 
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	var height = $('.image-wrapper').height();
 	$('.image-wrapper').css({
 		'width': '600px',
@@ -30,5 +31,41 @@ $(document).ready(function(){
 
 	$("#exampleFormModal3 input[type='phone']").mask('(000) 000-00-00');
 
+
+	$.map( $('.datepicker-input'), function( val, i ) {
+
+	   	$(val).datepicker({
+			language: "ru",
+			orientation: "bottom"
+			});
+	});
+
+
+
+	var options = {
+		show2400: true,
+		timeFormat: 'H:i'
+		// time from & to
+	};
+
+	
+	$.map( $('.timepicker-input'), function( val, i ) {
+
+	   	$(val).timepicker(options);
+	});
+
+	$.map( $("select[name='month']"), function( val, i ) {
+
+
+	   	$(val).select2({
+	  		placeholder: function(){
+		        $(val).data('placeholder');
+		    },
+	  		minimumResultsForSearch: Infinity,
+	  		 width: 'element'
+		});
+
+	});
+	
 
 })
