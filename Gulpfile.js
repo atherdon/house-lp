cd1715da7feacff69074ff0ca745b2c1caa11901
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-08-07 01:26:47 +0300

    +

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,40 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var bootstrap = require('bootstrap-styl');
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
+var gulp        = require('gulp');
+var stylus      = require('gulp-stylus');
+var bootstrap   = require('bootstrap-styl');
+var concat      = require('gulp-concat');
+var cssmin      = require('gulp-cssmin');
+var rename      = require('gulp-rename');
 var browserSync = require('browser-sync');
 
 gulp.task('stylus', function() {
-	return gulp.src('assets/stylus/style.styl')
-				.pipe(stylus({
-						use: [
-							bootstrap()
-						]
-				}))
-				.pipe(cssmin())
-				.pipe(rename({suffix: '.min'}))
-				.pipe(gulp.dest('assets/css'));
+	return gulp.src([
+			'https://fonts.googleapis.com/css?family=Roboto:300,400',
+			'http://cdn.jsdelivr.net/bxslider/4.1.1/jquery.bxslider.css',
+			'assets/css/jquery.timepicker.css',
+			'assets/stylus/style.styl'
+		])
+		.pipe(stylus({
+				use: [
+					bootstrap()
+				]
+		}))
+		.pipe(concat('style.css'))
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('assets/css'));
 });
+
+// gulp.task('stylus', function() {
+// 	return gulp.src('assets/stylus/style.styl')
+// 				.pipe(stylus({
+// 						use: [
+// 							bootstrap()
+// 						]
+// 				}))
+// 				.pipe(cssmin())
+// 				.pipe(rename({suffix: '.min'}))
+// 				.pipe(gulp.dest('assets/css'));
+// });
 
 gulp.task('browserSync', ['stylus'], function(){
 	browserSync.init({
