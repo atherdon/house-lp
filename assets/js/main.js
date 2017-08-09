2adde82fc77d52aa16b17e29140928d209102cdb
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-08-10 01:08:01 +0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
 
-	var height = $('.image-wrapper').height();
-	$('.image-wrapper').css({
-		'width': '600px',
-		'height': '600px'
-	});
+//
 
 	//nivo lightbox init
 
